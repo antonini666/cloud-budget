98cf643eeb5b9ac2964748660c098b75301a32ae
antonini666 <anton.yanovskyi@gmail.com>
2020-01-21 23:46:59 +0200

    add burger menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
+const $ = element => {
+  return document.querySelector(element);
+};
+
+//create burger menu
+
+let menuToggler = $(".menu__btn");
+let body = $("body");
+
+menuToggler.addEventListener("click", () => {
+  body.classList.toggle("open");
+});
+
+//init AOS
 AOS.init();
